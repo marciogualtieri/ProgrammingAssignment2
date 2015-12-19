d0db7103e0ab96c31c2383ff889df24deab2eec7
Marcio Gualtieri <marcio.gualtieri@gmail.com>
2015-12-19 14:27:28 +0000

    Refactored unit tests

--- cachematrix.R
+++ cachematrix.R
@@ -14,7 +14,7 @@
 ## cacheSolve(cached_test_matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
-  ## This function creates a special "matrix" object that can cache its inverse.
+  ## Creates a special "matrix" object that can cache its inverse.
   ##
   ## Args:
   ## x: An invertible matrix
@@ -42,7 +42,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 cacheSolve <- function(x, ...) {
-  ## This function computes the inverse of the  special "matrix" returned by makeCacheMatrix above.
+  ## Computes the inverse of the  special "matrix" returned by makeCacheMatrix above.
   ## If the inverse has already been calculated (and the matrix has not changed), then cachesolve()
   ## should retrieve the inverse from the cache.
   ##
--- tests/cachematrix.test.R
+++ tests/cachematrix.test.R
@@ -1,21 +1,31 @@
 test.cachematrix <- function()
 {
-  # set test conditions and expectations
-  test_matrix_size <- 200
-  decimal_places_to_compare <- 3
-  test_matrix <- matrix(rexp(test_matrix_size ^ 2), test_matrix_size, test_matrix_size)
-  solve_benchmarks <- benchmark(expected_inverse_test_matrix <- solve(test_matrix))
-  cat(sprintf('\nsolve() computation time: %f\n', solve_benchmarks$elapsed))
-  
-  # compute inverse using caching
-  cached_test_matrix <- makeCacheMatrix(test_matrix)
-  cache_solve_benchmarks <- benchmark(inverse_test_matrix <- cacheSolve(cached_test_matrix))
-  cat(sprintf('\ncacheSolve() computation time: %f\n', cache_solve_benchmarks$elapsed))
-  
-  # check if method returns the expected result first
-  expect_that(round(inverse_test_matrix, decimal_places_to_compare), equals(round(expected_inverse_test_matrix, decimal_places_to_compare
-  )))
-  
-  # check if benchmarks improved for caching
-  expect_less_than(cache_solve_benchmarks$elapsed, solve_benchmarks$elapsed)
+  given_the_test_setup()
+  when_i_compute_the_inverse_matrix_using_caching()
+  then_i_get_the_correct_inverse_matrix()
+  then_caching_is_faster() 
+}
+
+given_the_test_setup <- function() {
+  TEST_MATRIX_SIZE <<- 200
+  DECIMAL_PLACES_TO_COMPARE <<- 3
+  TEST_MATRIX <<- matrix(rexp(TEST_MATRIX_SIZE ^ 2), TEST_MATRIX_SIZE, TEST_MATRIX_SIZE)
+  SOLVE_BENCHMARKS <<- benchmark(EXPECTED_INVERSE_TEST_MATRIX <<- solve(TEST_MATRIX))
+  cat(sprintf('\nsolve() computation time: %f\n', SOLVE_BENCHMARKS$elapsed))  
+}
+
+when_i_compute_the_inverse_matrix_using_caching <- function() {
+  CACHED_TEST_MATRIX <<- makeCacheMatrix(TEST_MATRIX)
+  CACHE_SOLVE_BENCHMARKS <<- benchmark(INVERSE_TEST_MATRIX <<- cacheSolve(CACHED_TEST_MATRIX))
+  cat(sprintf('\ncacheSolve() computation time: %f\n', CACHE_SOLVE_BENCHMARKS$elapsed))
+}
+
+then_i_get_the_correct_inverse_matrix <- function() {
+  expect_that(round(INVERSE_TEST_MATRIX, DECIMAL_PLACES_TO_COMPARE),
+              equals(round(EXPECTED_INVERSE_TEST_MATRIX, DECIMAL_PLACES_TO_COMPARE)))
+}
+
+then_caching_is_faster <- function() {
+  expect_less_than(CACHE_SOLVE_BENCHMARKS$elapsed,
+                   SOLVE_BENCHMARKS$elapsed)
 }
